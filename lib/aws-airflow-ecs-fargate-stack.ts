4ad7b54ff573754168b9057abfa61e1627401d01
Mat Werber <werberm@amazon.com>
2020-02-29 13:07:17 -0800

    add container task definitions

--- lib/aws-airflow-ecs-fargate-stack.ts
+++ lib/aws-airflow-ecs-fargate-stack.ts
@@ -3,22 +3,27 @@ import rds = require('@aws-cdk/aws-rds');
 import secretsmanager = require('@aws-cdk/aws-secretsmanager');
 import ec2 = require("@aws-cdk/aws-ec2");
 import ecs = require("@aws-cdk/aws-ecs");
-import ecs_patterns = require("@aws-cdk/aws-ecs-patterns");
-
+import s3 = require("@aws-cdk/aws-s3");
+import { TaskDefinition } from '@aws-cdk/aws-ecs';
+import { DockerImageAsset } from '@aws-cdk/aws-ecr-assets';
+import { RemovalPolicy } from '@aws-cdk/core';
+var fs = require('fs');
+var path = require('path');
 
 const CFG = {
   db: {
     databaseName: 'airflow',
-    dbClusterIdentifier: 'airflow',
-    engine: 'aurora',
+    dbClusterIdentifier: 'aurora-airflow',
+    engine: 'aurora-postgresql',
     engineMode: 'serverless',
-    masterUsername: 'admin',
-    port: 3306,
+    masterUsername: 'airflow',
+    port: 5432,
     autoPause: false,
-    minCapacity: 1, 
+    minCapacity: 2, 
     maxCapacity: 8,
     SecondsUntilAutoPause: 3600,
-    subnetIds: ['subnet-0cc5bd19c2c1829aa', 'subnet-02b4e00939e9f33bc'] // list of pre-existing subnet IDs
+    subnetIds: ['subnet-0cc5bd19c2c1829aa', 'subnet-02b4e00939e9f33bc'], // list of pre-existing subnet IDs
+    vpcSecurityGroupIds: ['sg-00e88c6dc027cc7ce']
   },
   ecs: {
     vpcId: 'vpc-23d0fe58'
@@ -29,34 +34,35 @@ export class AwsAirflowEcsFargateStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    
-    // Generate an AWS Secrets Manager secret that will contain database username and password: 
-    const databaseSecret = new secretsmanager.Secret(this, 'DatabaseSecret', {
-      generateSecretString: {
-        secretStringTemplate: JSON.stringify({ username: CFG.db.masterUsername }),
-        generateStringKey: 'password',
-        excludeCharacters: '@/" ',     // these chars not allowed: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_Limits.html
-        includeSpace: false, // not allowed for RDS master password
-        passwordLength: 30
-      }
+    const databasePasswordSecret = new secretsmanager.Secret(this, 'AirflowDatabasePassword', {
+      secretName: "airflow/postgres/password"
     });
+
+    // S3 Bucket to which we will ship airflow logs: 
+    const airflowLogBucket = new s3.Bucket(this, 'airflowLogBucket', {
+      removalPolicy: RemovalPolicy.DESTROY
+    });
+
+    var s3_log_path = "s3://" + airflowLogBucket.bucketName + "/logs"
 
     // Define the subnet group which our Aurora cluster will use: 
     const dbSubnetGroup = new rds.CfnDBSubnetGroup(this, 'DatabaseSubnetGroup', {
       dbSubnetGroupDescription: 'Subnet group for Airflow database',
+      dbSubnetGroupName: 'airflow-db-subnet-group',
       subnetIds: CFG.db.subnetIds
     });
-
+    
     // The Aurora database that our airflow service will use:
-    const aurora = new rds.CfnDBCluster(this, 'AuroraServerless', {
+    const aurora = new rds.CfnDBCluster(this, 'AuroraAirflow', {
       databaseName: CFG.db.databaseName,
       dbClusterIdentifier: CFG.db.dbClusterIdentifier,
       engine: CFG.db.engine,
       engineMode: CFG.db.engineMode,
       masterUsername: CFG.db.masterUsername,
-      masterUserPassword: databaseSecret.secretValueFromJson('password').toString(),
+      masterUserPassword: databasePasswordSecret.secretValue.toString(),
       port: CFG.db.port,
       dbSubnetGroupName: dbSubnetGroup.dbSubnetGroupName,
+      vpcSecurityGroupIds: CFG.db.vpcSecurityGroupIds,
       scalingConfiguration: {
         autoPause: CFG.db.autoPause,
         maxCapacity: CFG.db.maxCapacity,
@@ -65,6 +71,9 @@ export class AwsAirflowEcsFargateStack extends cdk.Stack {
       }
     });
 
+    aurora.node.addDependency(dbSubnetGroup);
+
+    
     // The vpc that our ECS Fargate tasks will use: 
     const ecsVpc = ec2.Vpc.fromLookup(this, 'ecsVpc', {
       vpcId: CFG.ecs.vpcId
@@ -75,5 +84,121 @@ export class AwsAirflowEcsFargateStack extends cdk.Stack {
       vpc: ecsVpc
     });
 
+    
+    const airflowImage = new DockerImageAsset(this, 'airflowImage', {
+      directory: path.join(__dirname, 'docker')
+    });
+    
+    const webserverTaskDefinition = new ecs.FargateTaskDefinition(this, 'webserverTaskDefinition', {
+      family: 'airflow_webserver',
+      cpu: 512,
+      memoryLimitMiB: 1024
+    });
+
+    webserverTaskDefinition.addContainer('DefaultContainer', {
+      image: ecs.ContainerImage.fromEcrRepository(airflowImage.repository),
+      command: ['webserver'],
+      environment: {
+        POSTGRES_DB: CFG.db.databaseName, 
+        POSTGRES_HOST: aurora.attrEndpointAddress,
+        POSTGRES_PORT: aurora.attrEndpointPort,
+        POSTGRES_USER: CFG.db.masterUsername,
+        S3_LOG_PATH: s3_log_path
+      },
+      secrets: {
+        POSTGRES_PASSWORD: ecs.Secret.fromSecretsManager(databasePasswordSecret)
+      }
+    })
+      .addPortMappings({ containerPort: 8080 });
+    
+    
+    const schedulerTaskDefinition = new ecs.FargateTaskDefinition(this, 'schedulerTaskDefinition', {
+      family: 'airflow_scheduler',
+      cpu: 512,
+      memoryLimitMiB: 2048
+    });
+
+    schedulerTaskDefinition.addContainer('DefaultContainer', {
+      image: ecs.ContainerImage.fromEcrRepository(airflowImage.repository),
+      command: ['scheduler'],
+      environment: {
+        POSTGRES_DB: CFG.db.databaseName, 
+        POSTGRES_HOST: aurora.attrEndpointAddress,
+        POSTGRES_PORT: aurora.attrEndpointPort,
+        POSTGRES_USER: CFG.db.masterUsername,
+        REDIS_HOST: 'redis.airflow.celery',
+        S3_LOG_PATH: s3_log_path
+      },
+      secrets: {
+        POSTGRES_PASSWORD: ecs.Secret.fromSecretsManager(databasePasswordSecret)
+      }
+    });
+    
+    const flowerTaskDefinition = new ecs.FargateTaskDefinition(this, 'flowerTaskDefinition', {
+      family: 'airflow_flower',
+      cpu: 256,
+      memoryLimitMiB: 512
+    });
+
+    flowerTaskDefinition.addContainer('DefaultContainer', {
+      image: ecs.ContainerImage.fromEcrRepository(airflowImage.repository),
+      command: ['flower'],
+      environment: {
+        POSTGRES_DB: CFG.db.databaseName, 
+        POSTGRES_HOST: aurora.attrEndpointAddress,
+        POSTGRES_PORT: aurora.attrEndpointPort,
+        POSTGRES_USER: CFG.db.masterUsername,
+        S3_LOG_PATH: s3_log_path
+      },
+      secrets: {
+        POSTGRES_PASSWORD: ecs.Secret.fromSecretsManager(databasePasswordSecret)
+      }
+    })
+    .addPortMappings({ containerPort: 5555 });;
+
+    const workerTaskDefinition = new ecs.FargateTaskDefinition(this, 'workerTaskDefinition', {
+      family: 'airflow_worker',
+      cpu: 1024,
+      memoryLimitMiB: 3072
+    });
+
+    workerTaskDefinition.addContainer('DefaultContainer', {
+      image: ecs.ContainerImage.fromEcrRepository(airflowImage.repository),
+      command: ['worker'],
+      environment: {
+        POSTGRES_DB: CFG.db.databaseName, 
+        POSTGRES_HOST: aurora.attrEndpointAddress,
+        POSTGRES_PORT: aurora.attrEndpointPort,
+        POSTGRES_USER: CFG.db.masterUsername,
+        REDIS_HOST: 'redis.airflow.celery',
+        S3_LOG_PATH: s3_log_path
+      },
+      secrets: {
+        POSTGRES_PASSWORD: ecs.Secret.fromSecretsManager(databasePasswordSecret)
+      }
+    })
+    .addPortMappings({ containerPort: 8793 });;
+    
+    const redisTaskDefinition = new ecs.FargateTaskDefinition(this, 'redisTaskDefinition', {
+      family: 'airflow_redis',
+      cpu: 1024,
+      memoryLimitMiB: 2048
+    });
+
+    redisTaskDefinition.addContainer('DefaultContainer', {
+      image: ecs.ContainerImage.fromRegistry('docker.io/redis:5.0.5'),
+      environment: {
+        POSTGRES_DB: CFG.db.databaseName, 
+        POSTGRES_HOST: aurora.attrEndpointAddress,
+        POSTGRES_PORT: aurora.attrEndpointPort,
+        POSTGRES_USER: CFG.db.masterUsername,
+        S3_LOG_PATH: s3_log_path
+      },
+      secrets: {
+        POSTGRES_PASSWORD: ecs.Secret.fromSecretsManager(databasePasswordSecret)
+      }
+    })
+      .addPortMappings({ containerPort: 6379 });;
+    
   }
 }
---
+++ lib/docker/entrypoint.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+: "${REDIS_HOST:="redis"}"
+: "${REDIS_PORT:="6379"}"
+: "${REDIS_PASSWORD:=""}"
+
+: "${POSTGRES_HOST:="postgres"}"
+: "${POSTGRES_PORT:="5432"}"
+: "${POSTGRES_USER:="airflow"}"
+: "${POSTGRES_PASSWORD:="airflow"}"
+: "${POSTGRES_DB:="airflow"}"
+
+# Defaults and back-compat
+: "${AIRFLOW_HOME:="/usr/local/airflow"}"
+: "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}}"
+: "${AIRFLOW__CORE__EXECUTOR:=${EXECUTOR:-Celery}Executor}"
+
+AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
+AIRFLOW__CELERY__RESULT_BACKEND="db+postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
+
+
+export \
+  AIRFLOW_HOME \
+  AIRFLOW__CELERY__BROKER_URL \
+  AIRFLOW__CELERY__RESULT_BACKEND \
+  AIRFLOW__CORE__EXECUTOR \
+  AIRFLOW__CORE__FERNET_KEY \
+  AIRFLOW__CORE__LOAD_EXAMPLES \
+  AIRFLOW__CORE__SQL_ALCHEMY_CONN \
+
+
+# Load DAGs examples (default: Yes)
+if [[ -z "$AIRFLOW__CORE__LOAD_EXAMPLES" && "${LOAD_EX:=n}" == n ]]
+then
+  AIRFLOW__CORE__LOAD_EXAMPLES=False
+fi
+
+if [ -n "$REDIS_PASSWORD" ]; then
+    REDIS_PREFIX=:${REDIS_PASSWORD}@
+else
+    REDIS_PREFIX=
+fi
+
+AIRFLOW__CELERY__BROKER_URL="redis://$REDIS_PREFIX$REDIS_HOST:$REDIS_PORT/1"
+
+
+case "$1" in
+  webserver)
+    if [ -e "/requirements.txt" ]; then
+      pip3 install --user --upgrade -r /requirements.txt
+    fi
+
+    airflow initdb
+    exec airflow webserver
+    ;;
+  scheduler|flower|version)
+
+    exec airflow "$@"
+    ;;
+  worker)
+    if [ -e "/requirements.txt" ]; then
+      pip3 install --user --upgrade -r /requirements.txt
+    fi
+
+    exec airflow "$@"
+    ;;
+  *)
+    # The command is something like bash, not an airflow subcommand. Just run it in the right environment.
+    exec "$@"
+    ;;
+esac
